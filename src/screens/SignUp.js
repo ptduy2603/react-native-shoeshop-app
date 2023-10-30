4d1bda2f1b90bafbf5ee7d098d1310880d29d9a0
Thanh Duy <duythanhpham2603@gmail.com>
2023-10-30 16:38:02 +0700

    Complete registration feature

--- src/screens/SignUp.js
+++ src/screens/SignUp.js
@@ -1,8 +1,8 @@
-'use strict'
+'use strict';
 import { useReducer, useCallback } from 'react';
 import { View, Text, SafeAreaView, StyleSheet, Keyboard, Alert } from 'react-native';
 import { Fontisto, SimpleLineIcons, FontAwesome } from '@expo/vector-icons';
-import axios from 'axios'
+import axios from 'axios';
 
 import useValidate from '../hooks/useValidate';
 import FormContainer from '../components/FormContainer';
@@ -11,99 +11,102 @@ import FormInputFeild from '../components/FormInputFeild';
 import CustomButton from '../components/CustomButton';
 import NavigateQuestion from '../components/NavigateQuestion';
 import GlobalStyles from '../untils/GlobalStyles';
-import { validEmailRegex } from '../constants'
+import { validEmailRegex } from '../constants';
 
 const initState = {
     username: '',
-    email : '',
-    password : '',
-    confirmationPassword : '',
-}
+    email: '',
+    password: '',
+    confirmationPassword: '',
+};
 
 const reducer = (state, action) => {
-    switch(action.type) {
-        case 'SET_VALUE' : {
+    switch (action.type) {
+        case 'SET_VALUE': {
             return {
                 ...state,
-                [action.payload.input] : action.payload.data
-            }
+                [action.payload.input]: action.payload.data,
+            };
         }
-        case 'RESET_VALUE_ALL' : {
-            return initState
+        case 'RESET_VALUE_ALL': {
+            return initState;
         }
         default:
-             return state
+            return state;
     }
-}
+};
 
 function SignUp({ navigation }) {
-    const [state, dispatch] = useReducer(reducer, initState)
-    const { invalidFeilds, handleSetInvalidFeilds, handleResetInvalidFeilds, handleCheckInvalid } = useValidate()
+    const [state, dispatch] = useReducer(reducer, initState);
+    const { invalidFeilds, handleSetInvalidFeilds, handleResetInvalidFeilds, handleCheckInvalid } =
+        useValidate();
 
     const handleInputChange = (input, value) => {
         dispatch({
             type: 'SET_VALUE',
             payload: {
                 input,
-                data : value
-            }
-        })
-    } 
-    
+                data: value,
+            },
+        });
+    };
+
     const validateFormInput = useCallback(() => {
-        let check = true
-        Keyboard.dismiss()
-        if(!state.username.trim()) {
-            handleSetInvalidFeilds('username', 'Please enter your username')
-            check = false
+        let check = true;
+        Keyboard.dismiss();
+        if (!state.username.trim()) {
+            handleSetInvalidFeilds('username', 'Please enter your username');
+            check = false;
         }
-        if(state.confirmationPassword.trim() !== state.password.trim()) {
-            handleSetInvalidFeilds('confirmationPassword', 'Your confirmation is incorrect')
-            check = false
+        if (state.confirmationPassword.trim() !== state.password.trim()) {
+            handleSetInvalidFeilds('confirmationPassword', 'Your confirmation is incorrect');
+            check = false;
         }
-        if(!state.email.trim())
-        {
-            handleSetInvalidFeilds('email', 'Please enter your email')
-            check = false
+        if (!state.email.trim()) {
+            handleSetInvalidFeilds('email', 'Please enter your email');
+            check = false;
         }
-        if(!state.password.trim())
-        {
-            handleSetInvalidFeilds('password', 'Please enter your password')
-            check = false
+        if (!state.password.trim()) {
+            handleSetInvalidFeilds('password', 'Please enter your password');
+            check = false;
         }
-        if(!state.confirmationPassword.trim()) {
-            handleSetInvalidFeilds('confirmationPassword', 'Please confirm your password')
-            check = false
+        if (!state.confirmationPassword.trim()) {
+            handleSetInvalidFeilds('confirmationPassword', 'Please confirm your password');
+            check = false;
         }
         // check valid email's format
-        if(!state.email.trim().match(validEmailRegex)) {
-            handleSetInvalidFeilds('email', 'Your email is invalid')
-            check = false
+        if (!state.email.trim().match(validEmailRegex)) {
+            handleSetInvalidFeilds('email', 'Your email is invalid');
+            check = false;
         }
-        
-        return check            
-    }, [state, handleSetInvalidFeilds]) 
+
+        return check;
+    }, [state, handleSetInvalidFeilds]);
 
     const handleCreateNewAccount = useCallback(() => {
-        if(validateFormInput()) {
+        if (validateFormInput()) {
             const user = {
                 username: state.username,
-                email : state.email,
-                password : state.password,
-            }
+                email: state.email,
+                password: state.password,
+            };
             // send request to server to create new user
-            axios.get('http://localhost:8000/users/')
+            axios
+                .post('http://10.0.2.2:8000/users/register', user)
                 .then((response) => {
-                    console.log(response)
-                    // Alert.alert("Sign up successfully!", "Message")
-                    // dispatch({ type : 'RESET_VALUE_ALL' })                        
+                    console.log(response);
+                    Alert.alert("Sign up successfully!", "Message")
+                    dispatch({ type : 'RESET_VALUE_ALL' })
+
+                    // Loading effect and navigate to SignIn screen
+                    
                 })
                 .catch((error) => {
-                    // Alert.alert('Error message', 'Registration failed')
-                    console.log('Registration error: ', error)
-                })
+                    Alert.alert('Error message', 'Registration failed, your mail is existent')
+                    console.log('Registration error: ', error);
+                });
         }
-    }, [validateFormInput])
+    }, [validateFormInput]);
 
     return (
         <SafeAreaView style={GlobalStyles.container}>
@@ -115,14 +118,16 @@ function SignUp({ navigation }) {
                 <View style={styles.form}>
                     <View style={styles.formGroup}>
                         <FormInputFeild
-                            value={state.username}   
-                            isInvalid={handleCheckInvalid('username')}                    
+                            value={state.username}
+                            isInvalid={handleCheckInvalid('username')}
                             placeholder="User name"
                             icon={<FontAwesome name="user-o" size={26} color="black" />}
                             handleTextChange={(value) => handleInputChange('username', value)}
                             handleOnFocus={() => handleResetInvalidFeilds('username')}
                         />
-                        {handleCheckInvalid('username') && <Text style={styles.invalidMessage}>{invalidFeilds['username']}</Text>}
+                        {handleCheckInvalid('username') && (
+                            <Text style={styles.invalidMessage}>{invalidFeilds['username']}</Text>
+                        )}
                     </View>
                     <View style={styles.formGroup}>
                         <FormInputFeild
@@ -131,10 +136,12 @@ function SignUp({ navigation }) {
                             isInvalid={handleCheckInvalid('email')}
                             type="email"
                             icon={<Fontisto name="email" size={26} color="black" />}
-                            handleTextChange={(value) => handleInputChange('email',value)}
+                            handleTextChange={(value) => handleInputChange('email', value)}
                             handleOnFocus={() => handleResetInvalidFeilds('email')}
                         />
-                        {handleCheckInvalid('email') && <Text style={styles.invalidMessage}>{invalidFeilds['email']}</Text>}
+                        {handleCheckInvalid('email') && (
+                            <Text style={styles.invalidMessage}>{invalidFeilds['email']}</Text>
+                        )}
                     </View>
                     <View style={styles.formGroup}>
                         <FormInputFeild
@@ -146,7 +153,9 @@ function SignUp({ navigation }) {
                             handleTextChange={(value) => handleInputChange('password', value)}
                             handleOnFocus={() => handleResetInvalidFeilds('password')}
                         />
-                        {handleCheckInvalid('password') && <Text style={styles.invalidMessage}>{invalidFeilds['password']}</Text>}
+                        {handleCheckInvalid('password') && (
+                            <Text style={styles.invalidMessage}>{invalidFeilds['password']}</Text>
+                        )}
                     </View>
                     <View style={styles.formGroup}>
                         <FormInputFeild
@@ -155,18 +164,21 @@ function SignUp({ navigation }) {
                             isInvalid={handleCheckInvalid('confirmationPassword')}
                             isSecure
                             icon={<SimpleLineIcons name="lock" size={26} color="black" />}
-                            handleTextChange={(value) => handleInputChange('confirmationPassword', value)}
+                            handleTextChange={(value) =>
+                                handleInputChange('confirmationPassword', value)
+                            }
                             handleOnFocus={() => handleResetInvalidFeilds('confirmationPassword')}
                         />
-                        {handleCheckInvalid('confirmationPassword') && <Text style={styles.invalidMessage}>{invalidFeilds['confirmationPassword']}</Text>}
+                        {handleCheckInvalid('confirmationPassword') && (
+                            <Text style={styles.invalidMessage}>
+                                {invalidFeilds['confirmationPassword']}
+                            </Text>
+                        )}
                     </View>
                 </View>
 
                 <View style={{ marginTop: 20 }}>
-                    <CustomButton
-                        title="CREATE"
-                        handleOnPress={handleCreateNewAccount}
-                    />
+                    <CustomButton title="CREATE" handleOnPress={handleCreateNewAccount} />
                 </View>
 
                 <View style={{ marginTop: 30 }}>
@@ -192,7 +204,7 @@ const styles = StyleSheet.create({
     },
     invalidMessage: {
         fontSize: 13,
-        color : 'red',
+        color: 'red',
         marginTop: 2,
     },
 });
