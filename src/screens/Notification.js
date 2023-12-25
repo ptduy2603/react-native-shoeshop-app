48db52ec6503a6dce7d41666d6ef731c4ba1c053
Lê Quốc Anh <21520565@gm.uit.edu.vn>
2023-12-25 14:39:50 +0700

    delete comment

--- src/screens/Notification.js
+++ src/screens/Notification.js
@@ -1,6 +1,6 @@
 import { useState, useEffect } from 'react';
 import { View } from 'react-native';
-import { useSelector } from 'react-redux'; // Import useSelector
+import { useSelector } from 'react-redux';
 import { useNavigation } from '@react-navigation/native';
 
 import GlobalStyles from '../untils/GlobalStyles';
@@ -9,7 +9,7 @@ import NotiCard from '../components/NotiCard';
 const Notification = () => {
   const [notiCount, setNotiCount] = useState(0);
   const navigation = useNavigation();
-  const notifications = useSelector((state) => state.notifications.notifications); // Use Redux state
+  const notifications = useSelector((state) => state.notifications.notifications);
 
   useEffect(() => {
     setNotiCount(notifications.length);
